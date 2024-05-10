1c01bff4183f522b8ddb229d93ba613f634b1671
Allan-Victor <akibuuka50@gmail.com>
2024-05-10 17:47:46 +0300

    implemented jwt Security

---
+++ book-network/src/main/java/com/allan/book/user/Token.java
@@ -0,0 +1,35 @@
+package com.allan.book.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Token {
+
+    @Id
+    @SequenceGenerator(name = "token_seq",
+    sequenceName = "token_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "token_seq")
+    private Integer id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
